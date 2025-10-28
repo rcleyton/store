119dc4784c5eed00a8bf937768193ae079cbdf1c
Cleyton Silva <silva.rcleyton@gmail.com>
2025-10-28 12:35:03 -0300

    Resolving cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  
   resources :products
+  
+  resource :cart, only: %i[ create show ] do
+    post :add_item
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
